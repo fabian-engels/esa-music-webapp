577c15388704994533f80858b159eac7aeb6dbb7
engels.fabian <engels.fabian@67675a90-d328-9a92-79f7-1c8e31cca7cb>
2013-01-28 01:01:23 +0000

    added credentials are required messages

--- EsaMusicWebapp/src/main/java/de/esamusicwebapp/core/services/userauth/LoginBean.java
+++ EsaMusicWebapp/src/main/java/de/esamusicwebapp/core/services/userauth/LoginBean.java
@@ -11,27 +11,30 @@ import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 /**
  *
  * @author nto
  */
 @ManagedBean
-@SessionScoped 
-public class LoginBean implements Serializable{
+@SessionScoped
+public class LoginBean implements Serializable {
 
     private String username;
     private String password;
     private boolean loggedin;
-    @EJB(mappedName="UserAuth")
+    @EJB(mappedName = "UserAuth")
     UserAuth userAuth;
 
     public LoginBean() {
         loggedin = false;
     }
-    
+
     /**
      * Get the value of password
      *
@@ -67,49 +70,67 @@ public class LoginBean implements Serializable{
     public void setUsername(String username) {
         this.username = username;
     }
+    
+    public void displayMessage() {
+        FacesContext.getCurrentInstance().addMessage("loginMessage", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Sample error message", "PrimeFaces makes no mistakes"));
+    }
 
     /**
      * Login based on username and password then redirect.
-     * 
+     *
      * @return the value of redirection target
      */
     public String login() {
-        if(username.isEmpty()||password.isEmpty()){
+        if (username.isEmpty() || password.isEmpty()) {
+            Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, "Required name or password are missing.");
+            FacesContext.getCurrentInstance().addMessage("loginMessage", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Empty value", "Username or password are empty."));
             return "index";
-        }else{
-            UserObject login=null;
-            try {
-                login = userAuth.login(this.username, this.password);
-            } catch (IllegalNameException ex) {
-                Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, null, ex);
-                Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, ex.getMessage());
-            }
-            if(login!=null){
-                loggedin=true;
-            }
         }
-        
-        if(loggedin){
+        UserObject usrObj = null;
+        try {
+            usrObj = userAuth.login(this.username, this.password);
+        } catch (IllegalNameException ex) {
+            FacesContext.getCurrentInstance().addMessage("loginMessage", new FacesMessage(FacesMessage.SEVERITY_ERROR, "IllegalNameException", "Bad name."));
+            Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, null, ex);
+            Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, ex.getMessage());
+        }
+
+        loggedin = checkCredentials(usrObj);
+
+        if (loggedin) {
             return "search";
-        }else{
+        } else {
+            FacesContext.getCurrentInstance().addMessage("loginMessage", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Credentials check failed","Please check username and password."));
             return "index";
         }
     }
-    
+
+    /**
+     * Test credentials of current text input values against a user object.
+     *
+     * @param usrObj
+     * @return
+     */
+    private boolean checkCredentials(UserObject usrObj) {
+        return usrObj == null && (usrObj.getName().equals(this.username) && usrObj.getPassword().equals(this.password));
+    }
+
     public String register() {
-        UserObject register = null;
+        UserObject usrObj = null;
         try {
-            Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, "Username: " + username);
-            Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, "Username: " + password);
-            register = userAuth.register(this.username, this.password);
+            Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, "Username: {0}", username);
+            Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, "Username: {0}", password);
+            usrObj = userAuth.register(this.username, this.password);
         } catch (IllegalNameException ex) {
+            /* login failed */
             Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
             Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, ex.getMessage());
         }
-        if(register!=null) {
+        if (checkCredentials(usrObj)) {
+            /* login successful */
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Sample error message", "PrimeFaces makes no mistakes"));
             Logger.getLogger(LoginBean.class.getName()).log(Level.INFO, "Register successful");
         }
         return "index";
     }
-    
 }
