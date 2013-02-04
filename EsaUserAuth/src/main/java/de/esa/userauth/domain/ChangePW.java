094f6204d0961bfea89a4bacc1697159af49783f
engels.fabian <engels.fabian@67675a90-d328-9a92-79f7-1c8e31cca7cb>
2013-02-04 16:52:30 +0000

    new import changed structure UserAuthRemoteInterface is now used

---
+++ EsaUserAuth/src/main/java/de/esa/userauth/domain/ChangePW.java
@@ -0,0 +1,72 @@
+package de.esa.userauth.domain;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@MessageDriven(mappedName = "JMSTopic", activationConfig = {
+    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+    @ActivationConfigProperty(propertyName = "destination", propertyValue = "JMSTopic")
+})
+public class ChangePW implements MessageListener {
+
+    @PersistenceContext(name = "persUnit")
+    private EntityManager em;
+    
+    private static final Logger LOGGER = Logger.getLogger(ChangePW.class.getName());
+    
+    private UserObject getUserByName(final String username) {
+        LOGGER.log(Level.INFO, "Username: {0}", username);
+        final List<UserObject> resultList = em.createQuery("Select a From UserObject a", UserObject.class).getResultList();
+
+        for (UserObject next : resultList) {
+            if (next.getName().equals(username)) {
+                return next;
+            }
+        }
+        return null;
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        if (!(message instanceof TextMessage)) {
+            throw new IllegalArgumentException(
+                    "TOPIC Message is not of class " + TextMessage.class.getCanonicalName() + ": " + message);
+        }
+        final TextMessage msg = (TextMessage) message;
+        try {
+            final String messageText = msg.getText();
+            LOGGER.log(Level.INFO, " TOPIC received: {0}", messageText);
+            
+            if (messageText.matches("changepw:.+?:.+?")) {
+                LOGGER.log(Level.INFO, "ATTEMPTING TO CHANGE PASSWORD");
+                final String[] mesArr = messageText.split(":");
+                final UserObject user = getUserByName(mesArr[1]);
+
+                user.setPassword(mesArr[2]);
+                LOGGER.log(Level.INFO, "PASSWORD CHANGED");
+
+            } else if (messageText.matches("deleteuser:.+?")) {
+                LOGGER.log(Level.INFO, "ATTEMPTING TO DELETRE USER");
+                final String[] mesArr = messageText.split(":");
+                final UserObject user = getUserByName(mesArr[1]);
+
+                em.remove(user);
+                LOGGER.log(Level.INFO, "USER DELETED");
+            }
+          //  Thread.sleep(1000); //Don't do such waiting in production!
+            LOGGER.log(Level.INFO, " TOPIC processed: {0}", messageText);
+        } catch (JMSException ex) {
+           // LOGGER.log(Level.WARNING, " TOPIC Failure processing message: " + msg, ex);
+        }
+    }
+}
---
+++ EsaUserAuth/src/main/java/de/esa/userauth/domain/UserAuth.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.esa.userauth.domain;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Stateless;
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+/**
+ *
+ * @author nto
+ */
+@Startup
+@Singleton
+public class UserAuth implements UserAuthRemote {
+    private static final Logger LOGGER = Logger.getLogger(UserAuth.class.getName());
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    public UserAuth() {
+        try {
+            LOGGER.log(Level.WARNING, "### Getting JNDI Resources ###");
+            InitialContext ict = new InitialContext();
+            NamingEnumeration children = ict.list("");
+            while(children.hasMoreElements()){
+                NameClassPair ncPair = (NameClassPair)children.next();
+                LOGGER.log(Level.WARNING, ncPair.getName() + " (type ");
+                LOGGER.log(Level.WARNING, ncPair.getClassName() + ")");
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public UserObject login(final String username, final String password) throws IllegalUsernameException {
+         LOGGER.log(Level.INFO, "login: Username: " + username + " Password: " + password);
+        final UserObject user = getUserByName(username);
+
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }else{
+            throw new IllegalUsernameException("Unknown username or password.");
+            /* For more security we throw no username or password specific exception. */
+        }
+    }
+
+    @Override
+    public UserObject register(final String username, final String password) throws IllegalUsernameException {
+        if(getUserByName(username) != null){
+            throw new IllegalUsernameException("Username already in use.");
+        }else{
+            em.persist(new UserObject(username, password));
+            return login(username, password);
+        }
+    }
+
+    @Override
+    public UserObject getUserByName(final String username) {
+        LOGGER.log(Level.INFO, "Username: " + username);
+        final List<UserObject> resultList = em.createQuery("Select a From UserObject a", UserObject.class).getResultList();
+
+        for (UserObject next : resultList) {
+            if (next.getName().equals(username)) {
+                return next;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteUser(final UserObject user) {
+        // @TODO throw UserNotFoundException
+        if (user == null) {
+            throw new IllegalArgumentException("user-UserObject-Object is NULL.");
+        }
+
+        final String username = user.getName();
+
+        if (user.getId() < 0) {
+            throw new IllegalArgumentException("user-Object has an illegal ID (<0)");
+        }
+
+        final UserObject userToDelete = em.find(UserObject.class, user.getId());
+
+        em.getTransaction().begin();
+        em.remove(userToDelete);
+        em.getTransaction().commit();
+
+        em.close();
+    }
+}
